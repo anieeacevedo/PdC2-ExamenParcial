5c1aca6f0e126243e1de3123a624229224cccc5c
A.L <aniee.acevedo@unmsm.edu.pe>
2026-05-29 11:12:10 -0500

    Delete directory0

--- VentaDeEntradas_V2/src/main/java/Modelo/LimiteEntradasException.java
+++
@@ -1,8 +0,0 @@
-package Modelo;
-
-// Excepción si se intentan comprar más de 4 entradas en una sola transacción
-public class LimiteEntradasException extends Exception {
-    public LimiteEntradasException(String mensaje) {
-        super(mensaje);
-    }
-}
